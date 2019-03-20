09ac480ea7706aa797e8310c3acceb9d11ee07ed
Avitesh <er.avitesh@gmail.com>
2019-03-19 21:21:24 -0500

    initial project commit

---
+++ src/main/java/com/project/brownie/AviteshAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.brownie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviteshAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviteshAppApplication.class, args);
+	}
+
+}
